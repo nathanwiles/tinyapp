31ccd72afbd533312409baad062b0f866b465275
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-25 02:25:24 -0400

    import database on start, save on close tweak comments

--- express_server.js
+++ express_server.js
@@ -1,75 +1,91 @@
-/**
- * created by Nathan Wiles
- * tinyapp project
- */
+/*
+* Written by: Nathan Wiles
+* Description: This file contains the server code for the TinyApp project.
+* It is responsible for handling all requests and responses.
+* It imports its database from the database.json file and saves it on server shutdown.
+*/
 
-// import Database
-const { urlDatabase } = require("./data/urlDatabase");
 const { generateTinyURL } = require("./helpers/generateTinyURL");
-// Setup server
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
+const { parse } = require("path");
+const fs = require("fs");
 
-// get database
+// Import database
+fs.readFile("./data/database.json", (err, data) => {
+  if (err) console.log(err);
 
+  // if no error, assign parsed data and log it
+  const urlDatabase = JSON.parse(data);
+  console.log("Imported Database:\n", urlDatabase);
 
+  // Setup server
+  const app = express();
+  const PORT = 8080; // default port 8080
+  app.set("view engine", "ejs");
 
+  // Middleware
+  app.use(express.urlencoded({ extended: true }));
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
+  // GET requests
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 
-// GET requests
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello!");
+  });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+  app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+  app.get("/hello", (req, res) => {
+    res.send("<html<body>Hello <b>World</b></body></html>\n");
+  });
 
-app.get("/hello", (req, res) => {
-  res.send("<html<body>Hello <b>World</b></body></html>\n");
-});
+  app.get("/urls/:id", (req, res) => {
+    if (!urlDatabase[req.params.id]) {
+      res.status(404);
+      res.render("urls_404");
+    } else {
+      const templateVars = {
+        id: req.params.id,
+        longURL: urlDatabase[req.params.id],
+      };
+      res.render("urls_show", templateVars);
+    }
+  });
 
-app.get("/urls/:id", (req, res) => {
-  if (!urlDatabase[req.params.id]) {
-    res.status(404);
-    res.render("urls_404");
-  } else {
-    const templateVars = {
-      id: req.params.id,
-      longURL: urlDatabase[req.params.id],
-    };
-    res.render("urls_show", templateVars);
-  }
-});
+  app.get("/u/:id", (req, res) => {
+    longURL = urlDatabase[req.params.id];
+    res.redirect(longURL);
+  });
 
-app.get("/u/:id", (req, res) => {
-  longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
+  // Post requests
+  app.post("/urls", (req, res) => {
+    const newLongURL = req.body.longURL;
+    const newTinyURL = generateTinyURL();
+    urlDatabase[newTinyURL] = newLongURL;
 
-// Post requests
-app.post("/urls", (req, res) => {
-  const newLongURL = req.body.longURL;
-  const newTinyURL = generateTinyURL();
-  urlDatabase[newTinyURL] = newLongURL;
+    res.redirect(`/urls/${newTinyURL}`);
+  });
 
-  res.redirect(`/urls/${newTinyURL}`);
-});
+  // Listen for requests
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
 
-// Listen for requests
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  // Save database on server shutdown
+  ["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) => {
+    process.on(signal, () => {
+      console.log(`Received ${signal}, saving database...`);
+      fs.writeFileSync("./data/database.json", JSON.stringify(urlDatabase));
+      process.exit(0);
+    });
+  });
 });
