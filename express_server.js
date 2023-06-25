8df246097d228f24934f34477bef43c7802f1a72
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-24 23:31:15 -0400

    format and lint

--- express_server.js
+++ express_server.js
@@ -7,38 +7,38 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
 
+// data base
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 // generate random string function
 const generateTinyURLObject = function() {
-  let tinyURL = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let tinyURL = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
-    const randomChar = characters[randomIndex]
+    const randomChar = characters[randomIndex];
     tinyURL += randomChar;
   }
-  const tinyObject = { tinyURL }
+  const tinyObject = { tinyURL };
   return tinyObject;
 };
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 
-
 // GET requests
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req,res) => {
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
@@ -51,17 +51,21 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html<body>Hello <b>World</b></body></html>\n")
+  res.send("<html<body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars)
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
 });
+
 // Post requests
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  const newTinyURL = generateTinyURLObject()
+  const newTinyURL = generateTinyURLObject();
   console.log(newTinyURL);
   res.send("Ok"); // temporary response
 });
