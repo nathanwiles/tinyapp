d0d46aa3592dae80263bf7d02e9e0bdc0f8725d8
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-24 22:38:50 -0400

    add .use for decoding POST buffers add comments

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@
  * tinyapp project
  */
 
+// Setup server
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -14,6 +15,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+
+// GET requests
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
@@ -39,6 +44,9 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars)
 });
+// End of GET requests
+
+// Listen for requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
