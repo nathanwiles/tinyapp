ab56df93de3cb3f3f8557759667066d58b49fed2
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-25 21:51:33 -0400

    fix empty database bug

--- express_server.js
+++ express_server.js
@@ -14,116 +14,125 @@ const saveDatabase = require("./helpers/saveDatabase");
 const databasePath = "./data/database.json";
 
 // Import database
-fs.readFile("./data/database.json", (err, data) => {
-  if (err) console.log(err);
-
-  // if no error, assign parsed data and log it
-  const urls = JSON.parse(data);
-  console.log("Imported Database:\n", urls);
-
-  // Setup server
-  const app = express();
-  const PORT = 8080; // default port 8080
-  app.set("view engine", "ejs");
+fs.readFile("./data/database.json", "utf-8", (err, data) => {
   
-  // Middleware
-  app.use(cookieParser());
-  app.use(express.urlencoded({ extended: true }));
+  let urls = {}; //object to store User urls
 
-  // GET requests
-  app.get("/urls", (req, res) => {
-    
-    const templateVars = { 
-      urls,
-      username : req.cookies["username"]
-    };
-    res.render("urls_index", templateVars);
-  });
-
-  app.get("/urls/new", (req, res) => {
-    const templateVars = { 
-      username : req.cookies["username"],
-    };
-    res.render("urls_new", templateVars);
-  });
-
-  app.get("/", (req, res) => {
-    const templateVars = { 
-      username : req.cookies["username"],
-    };
-    res.render("tinyapp_home");
-  });
-
-  app.get("/urls.json", (req, res) => {
-    res.json(urls);
-  });
-
-  app.get("/hello", (req, res) => {
-    res.send("<html<body>Hello <b>World</b></body></html>\n");
-  });
-
-  app.get("/urls/:id", (req, res) => {
-    const templateVars = { 
-      username : req.cookies["username"],
-    }
-    if (!urls[req.params.id]) {
-      res.status(404);
-      res.render("urls_404", templateVars);
+  if (err) {
+    console.log(err);
+  } else {
+    // if no error, assign parsed data and log it
+    if (!data) {
+      console.log("Source File Empty!\n");
+      urls = {};
     } else {
-      templateVars.id = req.params.id;
-      templateVars.longURL = urls[req.params.id];
+      if (data) urls = JSON.parse(data);
+      console.log("Imported Database:\n", urls);
+    }
+  
+
+    // Setup server
+    const app = express();
+    const PORT = 8080; // default port 8080
+    app.set("view engine", "ejs");
+
+    // Middleware
+    app.use(cookieParser());
+    app.use(express.urlencoded({ extended: true }));
+
+    // GET requests
+    app.get("/urls", (req, res) => {
+      const templateVars = {
+        urls,
+        username: req.cookies["username"],
       };
+      res.render("urls_index", templateVars);
+    });
+
+    app.get("/urls/new", (req, res) => {
+      const templateVars = {
+        username: req.cookies["username"],
+      };
+      res.render("urls_new", templateVars);
+    });
+
+    app.get("/", (req, res) => {
+      const templateVars = {
+        username: req.cookies["username"],
+      };
+      res.render("tinyapp_home");
+    });
+
+    app.get("/urls.json", (req, res) => {
+      res.json(urls);
+    });
+
+    app.get("/hello", (req, res) => {
+      res.send("<html<body>Hello <b>World</b></body></html>\n");
+    });
+
+    app.get("/urls/:id", (req, res) => {
+      const templateVars = {
+        username: req.cookies["username"],
+      };
+      if (!urls[req.params.id]) {
+        res.status(404);
+        res.render("urls_404", templateVars);
+      } else {
+        templateVars.id = req.params.id;
+        templateVars.longURL = urls[req.params.id];
+      }
       res.render("urls_show", templateVars);
-  });
+    });
 
-  app.get("/u/:id", (req, res) => {
-    longURL = urls[req.params.id];
-    res.redirect(longURL);
-  });
+    app.get("/u/:id", (req, res) => {
+      longURL = urls[req.params.id];
+      res.redirect(longURL);
+    });
 
-  // Post requests
-  app.post("/urls", (req, res) => {
-    const submittedLongURL = req.body.longURL;
-    const newLongURL = formatLongURL(submittedLongURL);
-    const newTinyURL = generateTinyURL();
+    // Post requests
+    app.post("/urls", (req, res) => {
+      const submittedLongURL = req.body.longURL;
+      const newLongURL = formatLongURL(submittedLongURL);
+      const newTinyURL = generateTinyURL();
 
-    urls[newTinyURL] = newLongURL;
+      urls[newTinyURL] = newLongURL;
 
-    console.log(`Received new tinyURL, saving database...`);
+      console.log(`Received new tinyURL, saving database...`);
 
-    saveDatabase(databasePath, urls);
+      saveDatabase(databasePath, urls);
 
-    res.redirect(`/urls/${newTinyURL}`);
-  });
+      res.redirect(`/urls/${newTinyURL}`);
+    });
 
-  app.post("/login", (req, res) => {
-    username = req.body.username;
-    res.cookie("username", username);
-    res.redirect("/urls");
-  });
+    app.post("/login", (req, res) => {
+      username = req.body.username;
+      res.cookie("username", username);
+      res.redirect("/urls");
+    });
 
+    app.post("/urls/:id/delete", (req, res) => {
+      const id = req.params.id;
+      delete urls[id];
+      console.log(`Deleted ${id} from database...`);
+      if (urls === undefined) urls = {};
+      saveDatabase(databasePath, urls);
+      res.redirect("/urls");
+    });
 
-  app.post("/urls/:id/delete", (req, res) => {
-    const id = req.params.id;
-    delete urls[id];
-    console.log(`Deleted ${id} from database...`);
-    saveDatabase(databasePath, urls);
-    res.redirect("/urls");
-  });
+    app.post("/urls/:id", (req, res) => {
+      const id = req.params.id;
+      const submittedLongURL = req.body.longURL;
+      const newLongURL = formatLongURL(submittedLongURL);
+      urls[id] = newLongURL;
+      console.log(`Updated ${id} in database...`);
+      saveDatabase(databasePath, urls);
+      res.redirect("/urls");
+    });
 
-  app.post("/urls/:id", (req, res) => {
-    const id = req.params.id;
-    const submittedLongURL = req.body.longURL;
-    const newLongURL = formatLongURL(submittedLongURL);
-    urls[id] = newLongURL;
-    console.log(`Updated ${id} in database...`);
-    saveDatabase(databasePath, urls);
-    res.redirect("/urls");
-  });
-
-
-  // Listen for requests
-  app.listen(PORT, () => {
-    console.log(`tinyapp listening on port ${PORT}!`);
-  });
+    // Listen for requests
+    app.listen(PORT, () => {
+      console.log(`tinyapp listening on port ${PORT}!`);
+    });
+  }
 });
