1c5a90008e471b48291b8f8026e52c714d49bec7
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-29 19:46:25 -0400

    change "username" varaible to "email"

--- express_server.js
+++ express_server.js
@@ -19,9 +19,9 @@ const {
 const databasePath = "./data/database.json";
 
 class User {
-  constructor(username, password) {
-    id = generateRandomString(10);
-    this.username = username;
+  constructor(email, password) {
+    this.id = generateRandomString(10);
+    this.email = email;
     this.password = password;
 
   }
@@ -36,21 +36,35 @@ fs.readFile("./data/database.json", "utf-8", (err, data) => {
   } else {
     // if no error, assign parsed data and log it
     if (!data) {
-      console.log("Source File Empty!\n");
+      console.log("URL Database empty!\n");
       urls = {};
     } else {
-      if (data) urls = JSON.parse(data);
-      console.log("Imported Database:\n");
+      urls = JSON.parse(data);
+      console.log("Imported URL Database:\n");
     }
   }
 });
+
+let users = {};
+fs.readFile("./data/user_data.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else if (!data) {
+    console.log("User Database empty!\n");
+    users = {};
+  } else {
+    users = JSON.parse(data);
+    console.log("Imported User Database:\n");
+  }
+});
+
 
 
 // Setup server
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-let username = null;
+let email = null;
 
 
 
@@ -58,7 +72,7 @@ let username = null;
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  username = req.cookies.username;
+  email = req.cookies.email;
   next();
 });
 
@@ -67,35 +81,35 @@ app.use((req, res, next) => {
 // GET requests
 app.get("/register", (req, res) => {
   const templateVars = {
-    username,
+    email,
   };
   res.render("user_register", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    urls: username !== null ? urls[username] : null,
-    username,
+    urls: email !== null ? urls[email] : null,
+    email,
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username,
+    email,
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/", (req, res) => {
   const templateVars = {
-    username,
+    email,
   };
   res.render("tinyapp_home", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urls.username);
+  res.json(urls.email);
 });
 
 app.get("/hello", (req, res) => {
@@ -104,21 +118,21 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    username,
+    email,
   };
   const id = req.params.id;
-  if (!urls[username][id]) {
+  if (!urls[email][id]) {
     res.status(404);
     res.render("urls_404", templateVars);
   } else {
     templateVars.id = id;
-    templateVars.longURL = urls[username][id];
+    templateVars.longURL = urls[email][id];
   }
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  longURL = urls[username][req.params.id];
+  longURL = urls[email][req.params.id];
   res.redirect(longURL);
 });
 
@@ -129,7 +143,7 @@ app.post("/urls", (req, res) => {
   const submittedLongURL = req.body.longURL;
   const newLongURL = formatLongURL(submittedLongURL);
   const newTinyURL = generateRandomString(6);
-  urls[username][newTinyURL] = newLongURL;
+  urls[email][newTinyURL] = newLongURL;
 
   console.log(`Received new tinyURL, saving database...`);
 
@@ -137,26 +151,37 @@ app.post("/urls", (req, res) => {
 
   res.redirect(`/urls/${newTinyURL}`);
 });
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = new User(email, password);
+  for (const user in users) {
+    if (user.email === email) {
+      res.status(400);
+      res.send("Username already exists!");
+    }
+  }
+  users[newUser.id] = newUser;
+  res.cookie("email", email);
+  res.redirect("/urls");
+});
+  
 
 app.post("/login", (req, res) => {
-  username = req.body.username;
-  if (!urls[username]) {
-    urls[username] = {};
-  }
-
-  res.cookie("username", username);
+  email = req.body.email;
+  res.cookie("email", email);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  username = null;
+  res.clearCookie("email");
+  email = null;
   res.redirect("/");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  delete urls[username][id];
+  delete urls[email][id];
   console.log(`Deleted ${id} from database...`);
   saveDatabase(databasePath, urls);
   res.redirect("/urls");
@@ -166,9 +191,9 @@ app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const submittedLongURL = req.body.longURL;
   const newLongURL = formatLongURL(submittedLongURL);
-  urls[username] ? urls[username] : (urls[username] = {}); // if user doesn't exist, create them-
-  urls[username][id] = newLongURL;
-  console.log(`Updated ${id} in ${username}'s database...`);
+  urls[email] ? urls[email] : (urls[email] = {}); // if user doesn't exist, create them-
+  urls[email][id] = newLongURL;
+  console.log(`Updated ${id} in ${email}'s database...`);
   saveDatabase(databasePath, urls);
   res.redirect("/urls");
 });
