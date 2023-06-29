2e35c04b66914740867dd7bb4dfdf9956809b849
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-28 22:47:41 -0400

    add helper to generalize rand. string creation

---
+++ helpers/generateRandomString.js
@@ -0,0 +1,16 @@
+/**
+ * @param {length} number representing the length of the string to be generated
+ * @returns {string} a random string of 10 characters
+ */
+
+const generateRandomString = function (length) {
+  let string = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length ; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    const randomChar = characters[randomIndex];
+    string += randomChar;
+  }
+  return string;
+}
