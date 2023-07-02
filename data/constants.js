5f899b30efdd5000fee264410fc5db21599a1e6d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-02 15:52:32 -0400

    move PORT to constants.js

--- data/constants.js
+++ data/constants.js
@@ -1,6 +1,6 @@
 const urlDatabasePath = "./data/database.json";
 const userDatabasePath = "./data/user_data.json";
+const PORT = 8080; // default port 8080
 
 
-
-module.exports = { urlDatabasePath, userDatabasePath };
+module.exports = { urlDatabasePath, userDatabasePath, PORT};
