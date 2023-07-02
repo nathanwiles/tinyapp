9e7f6287f8e83a269f701af74ea581dd8928b72d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-01 21:32:26 -0400

    move middleware into routes.

--- express_server.js
+++ express_server.js
@@ -3,9 +3,7 @@
  * Description: This file contains the server code for the TinyApp project.
  */
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const routes = require("./routes");
-
 
 // Setup server
 const app = express();
@@ -13,10 +11,9 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 let userId;
 
-// Middleware
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes,)
+// Utilize routes
+app.use(routes);
+
 // Listen for requests
 app.listen(PORT, () => {
   console.log(`tinyapp listening on port ${PORT}!`);
